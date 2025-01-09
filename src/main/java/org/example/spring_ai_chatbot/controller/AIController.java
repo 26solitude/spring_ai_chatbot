8c00dc67cf1ed6897db63e7ee104b5673d99cb5c
hojang <26dev@naver.com>
2025-01-09 13:52:00 +0900

    POST 형식으로 메시지 입력받아 대화 기능 추가

--- src/main/java/org/example/spring_ai_chatbot/controller/AIController.java
+++ src/main/java/org/example/spring_ai_chatbot/controller/AIController.java
@@ -1,9 +1,7 @@
 package org.example.spring_ai_chatbot.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,6 +17,14 @@ public class AIController {
     @GetMapping("/ai/chat")
     public Map<String, String> completion(@RequestParam(value = "message", defaultValue = "농담 하나 해보거라") String message) {
         return Map.of("달건이", this.chatClient.prompt().user(message).call().content());
+    }
+
+    @PostMapping("/ai/chat")
+    public String sendMessage(@RequestBody String userInput){
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
     }
 
 }
