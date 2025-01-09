60da80f739ee01d5a7a6fa93a95a461dc48a270e
hojang <26dev@naver.com>
2025-01-09 21:37:29 +0900

    프론트 구현

--- src/main/java/org/example/spring_ai_chatbot/controller/AIController.java
+++ src/main/java/org/example/spring_ai_chatbot/controller/AIController.java
@@ -4,8 +4,6 @@ import org.example.spring_ai_chatbot.config.Config;
 import org.example.spring_ai_chatbot.entity.ChatMessage;
 import org.example.spring_ai_chatbot.repository.ChatMessageRepository;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,32 +20,21 @@ public class AIController {
         this.chatMessageRepository = chatMessageRepository;
     }
 
-    @GetMapping("/ai/chat")
-    public Map<String, String> completion(@RequestParam(value = "message", defaultValue = "농담 하나 해보거라") String message) {
-        return Map.of("달건이", this.chatClient.prompt().user(message).call().content());
+    @GetMapping("/ai/chat/history")
+    public List<ChatMessage> getChatHistory(@RequestParam String userId) {
+        return chatMessageRepository.findByUserIdOrderByTimestampAsc((userId));
     }
 
     @PostMapping("/ai/chat")
-    public String sendMessage(@RequestParam String userId, @RequestBody String userInput) {
-
-        List<ChatMessage> history = chatMessageRepository.findByUserIdOrderByTimestampAsc(userId);
-
+    public Map<String, String> sendMessage(@RequestParam String userId, @RequestBody Map<String, String> request) {
         ChatClient.ChatClientRequestSpec prompt = chatClient.prompt().system(Config.DEFAULT_PROMPT);
-
-        for (var chat : history) {
-            if ("user".equals(chat.getRole())) {
-                prompt.messages(new UserMessage(chat.getContent()));
-            } else if ("assistant".equals(chat.getRole())) {
-                prompt.messages(new AssistantMessage(chat.getContent()));
-            }
-        }
-
+        String userInput = request.get("userInput");
         String aiResponse = prompt.user(userInput).call().content();
 
         chatMessageRepository.save(new ChatMessage(userId, "user", userInput));
         chatMessageRepository.save(new ChatMessage(userId, "assistant", aiResponse));
 
-        return aiResponse;
+        return Map.of("role", "assistant", "content", aiResponse);
     }
 }
 
